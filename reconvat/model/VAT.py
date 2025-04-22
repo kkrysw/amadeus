5cfa32fc060debb2f18a154c991c4414f1683cf5
Kevin Fu <kevinfu0319@gmail.com>
2025-04-22 18:55:30 -0400

    Update VAT.py

--- reconvat/model/VAT.py
+++ reconvat/model/VAT.py
@@ -40,5 +40,10 @@ class stepwise_VAT(nn.Module):
         return vat_loss, r_adv  # already averaged
     
 def _l2_normalize(d):
+    '''
     d = d/torch.norm(d, dim=2, keepdim=True)
     return d
+    '''
+    eps=1e-6
+    norm=torch.norm(d, dim=2, keepdim=True)
+    return d/(norm+eps)

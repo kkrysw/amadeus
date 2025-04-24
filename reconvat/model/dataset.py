ccfd00dccfaa7344485c9c63012addd6c320d8d8
TheApexWu <wua14@nyu.edu>
2025-04-24 01:13:07 -0400

    Revert "Update dataset.py"
    This reverts commit 5c3d96c0e04e1a58429cdb7e71c9e974091bd04b.

--- reconvat/model/dataset.py
+++ reconvat/model/dataset.py
@@ -150,7 +150,14 @@ class PianoRollAudioDataset(Dataset):
         velocity = torch.zeros(n_steps, n_keys, dtype=torch.uint8)
 
         tsv_path = tsv_path
-        midi = np.loadtxt(tsv_path, delimiter='\t', skiprows=1)
+        try:
+            midi = np.loadtxt(tsv_path, delimiter='\t', skiprows=1)
+            if midi.ndim == 1:
+                midi = np.expand_dims(midi, axis=0)
+        except Exception as e:
+            print(f"⚠Error reading {tsv_path}: {e}")
+            midi = np.zeros((0, 4))  # fallback: empty midi
+
         
 #         print(f'audio size = {audio.shape}')
 #         print(f'label size = {label.shape}')
